eb87fb16a380c161d601ae6d5f24deeabdc3a0e4
Walaff Fernandes <walaffonofre@icloud.com>
2024-02-27 15:01:43 -0300

    Adiciona página inicial com mensagem de boas-vindas e botão para começar - pt-br

---
+++ views/home.php
@@ -0,0 +1,11 @@
+<div class="flex justify-center items-center py-12 px-8">
+    <div class="text-center">
+        <h1 class="text-3xl font-bold mb-4">Bem-vindo ao Churras</h1>
+        <p class="mb-6">
+            O Churras é uma ferramenta para organizar encontros descontraídos com os amigos. Você pode criar um registro do seu churrasco, adicionar os participantes e listar os itens necessários para garantir uma experiência incrível. É uma maneira simples e eficaz de garantir que todos aproveitem ao máximo o momento juntos, sem preocupações!
+        </p>
+        <a href="/itens" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+            Vamos começar
+        </a>
+    </div>
+</div>
